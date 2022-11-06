50c15d996abfecf8d51be5d8861d01b404fb874b
fay <95728401+CoderFay@users.noreply.github.com>
2022-11-06 23:02:55 +0300

    Create PrintHello.js

---
+++ PrintHello.js
@@ -0,0 +1,12 @@
+//Simpler is impossible
+
+console.log('ry video http://goo.gl/aIoZM');
+
+console.log('__filename='+__filename);
+console.log('__dirname='+ __dirname);
+
+setTimeout(function(){
+	console.log('world!!');
+}, 2000);
+
+console.log('hello ')
